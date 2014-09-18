932a9a0f2785c5a4111cbdf096dc230b935ca448
edgemm-gavin <gavin@edgemm.com>
2014-09-18 11:22:59 -0700

    Initial Commit

---
+++ wp-content/themes/eagleview-child/footer.php
@@ -0,0 +1,10 @@
+		<div id="footer" class="clearfix">
+			<p id="copyright">Designed And Hosted by <a target="blank" href="http://www.edgemm.com">Portland Advertising Agency</a> Edge Multimedia</p>
+		</div> <!-- end #footer -->
+		
+	</div> <!-- end #container -->	
+
+	<?php get_template_part('includes/scripts'); ?>
+	<?php wp_footer(); ?>	
+</body>
+</html>
